c928ded48b172c21ccfd9a29da98f1c9c19ddfd1
Pavel <pvastig@gmail.com>
2019-10-12 03:05:58 +0700

    Delete unneeded header

--- tests/test.cpp
+++ tests/test.cpp
@@ -1,22 +1,21 @@
-#include "test.h"
-
 #include "../src/data.h"
 
 #include <gtest/gtest.h>
 
 TEST(CheckBraces, PositionResulut) {
-  ASSERT_EQ(Algo::checkBraces("]]]"), 1);
-  ASSERT_EQ(Algo::checkBraces("{"), 1);
-  ASSERT_EQ(Algo::checkBraces("]["), 1);
-  ASSERT_EQ(Algo::checkBraces("()"), 0);
-  ASSERT_EQ(Algo::checkBraces("(abc"), 1);
-  ASSERT_EQ(Algo::checkBraces("(a(bc"), 3);
-  ASSERT_EQ(Algo::checkBraces("[(a)]"), 0);
-  ASSERT_EQ(Algo::checkBraces("[(a)](((()))"), 6);
-  ASSERT_EQ(Algo::checkBraces("()[]}"), 5);
-  ASSERT_EQ(Algo::checkBraces("()()([}"), 7);
-  ASSERT_EQ(Algo::checkBraces("{{[()]]"), 7);
-  ASSERT_EQ(Algo::checkBraces("[]([]"), 3);
-  ASSERT_EQ(Algo::checkBraces("([)]"), 3);
-  ASSERT_EQ(Algo::checkBraces("(slkj{lk[lsj]}"), 1);
+  using namespace Algo;
+  ASSERT_EQ(checkBraces("]]]"), 1);
+  ASSERT_EQ(checkBraces("{"), 1);
+  ASSERT_EQ(checkBraces("]["), 1);
+  ASSERT_EQ(checkBraces("()"), 0);
+  ASSERT_EQ(checkBraces("(abc"), 1);
+  ASSERT_EQ(checkBraces("(a(bc"), 3);
+  ASSERT_EQ(checkBraces("[(a)]"), 0);
+  ASSERT_EQ(checkBraces("[(a)](((()))"), 6);
+  ASSERT_EQ(checkBraces("()[]}"), 5);
+  ASSERT_EQ(checkBraces("()()([}"), 7);
+  ASSERT_EQ(checkBraces("{{[()]]"), 7);
+  ASSERT_EQ(checkBraces("[]([]"), 3);
+  ASSERT_EQ(checkBraces("([)]"), 3);
+  ASSERT_EQ(checkBraces("(slkj{lk[lsj]}"), 1);
 }
